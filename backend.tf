d7977beded5fd480116f6010e66a7217eb9f37da
Syed Furkhan <you@your-domain.com>
2026-06-04 18:11:09 +0530

    Disable S3 backend for CI learning

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
-terraform {
-  backend "s3" {
-    bucket = "terra-tfstate-syed-001"
-    key    = "terraform.tfstate"
-    region = "ap-south-1"
-  }
-}
+# terraform {
+#   backend "s3" {
+#     bucket = "terra-tfstate-syed-001"
+#     key    = "terraform.tfstate"
+#     region = "ap-south-1"
+#   }
+# }
